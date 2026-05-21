f93337aa29bbd606c66121a54e73856a5a88424e
Mahendra Akula <159439793+akulamahendra@users.noreply.github.com>
2026-05-21 12:21:27 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,9 +1,8 @@
 terraform {
   required_providers {
     aws = {
-        source = "aws/hashicorp"
-        version = "~> 3.0"
-
+      source  = "hashicorp/aws"
+      version = "~> 3.0"
     }
   }
 }
